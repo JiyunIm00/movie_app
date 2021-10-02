a36a17c443e5f3e16e75f658b0ee19ef1fc2cc16
Jiyun Im <ellejiyunim@gmail.com>
2021-10-02 09:15:11 +0900

    #2-2 Prop-types

--- src/App.js
+++ src/App.js
@@ -1,51 +1,74 @@
-function Food({name, picture}){
-  // logs all props as an object
-  // console.log(prop);
-  return (
-      <div>
-        <h2>I like {name}</h2>
-        <img src={picture} />
-      </div>
-  );
-}
+import PropTypes from "prop-types";
 
 const foodILike = [
   {
+    id:1,
     name: "Kimchi",
     image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 4.9
   },
   {
+    id:2,
     name: "Samgyeopsal",
     image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+    rating: 4.8
   },
   {
+    id:3,
     name: "Bibimbap",
     image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+    rating: 5.0
   },
   {
+    id:4,
     name: "Doncasu",
     image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 4.7
   },
   {
+    id:5,
     name: "Kimbap",
     image:
       "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
   }
 ];
 
+function Food({name, picture, rating}){
+  return (
+      <div>
+        <h2>I like {name}</h2>
+        <h4>{rating}/5.0</h4>
+        <img src={picture} />
+      </div>
+  );
+}
 
-// Pass information to html (JSX) > prop = "", boolean, array ..etc.
-// can pass multiple props
-// map function: does something with each element in array and creates new array with return value
+// check if types are right
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  rating: PropTypes.number
+};
+
+// add key component b/c warning(only matters for react internal use)
+function renderFood(dish){
+  return <Food key = {dish.id} name = {dish.name} picture = {dish.image} />
+}
+
 function App() {
   return (
     <div className="App">
       {foodILike.map(dish => (
-        <Food name = {dish.name} picture = {dish.image} />
+        <Food
+          key={dish.id}
+          name={dish.name}
+          picture={dish.image}
+          rating={dish.rating}
+        />
       ))}
     </div>
   );
